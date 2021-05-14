fb03e0f1f96952728486a8601d76231262e6ae58
Darmian407 <damianmesa407@gmail.com>
2021-05-14 19:46:05 -0300

    nav provisorio y algunas cositas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+
 
 import {ButtonModule} from 'primeng/button';
 import {InputTextModule} from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 
 import {AccordionModule} from 'primeng/accordion';
+import { LoginComponent } from './login/login.component';
+
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +31,7 @@ import {AccordionModule} from 'primeng/accordion';
     ButtonModule,
     InputTextModule,
     AccordionModule,
+    MenubarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from "@angular/core";
+
+
+
+import {Router} from "@angular/router";
+
+
+@Component({
+    templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit{
+    private username: string;
+    private password: string;
+
+
+    constructor() {
+
+    }
+
+    ngOnInit():void {
+       
+      
+    }
+
+
+    
+}
